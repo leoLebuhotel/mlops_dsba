3168f5728c1b3e0f1e619f1d187cb021961490f7
Léo Lebuhotel <leo.lebuhotel@gmail.com>
2025-01-16 16:19:47 +0100

    preprocessing as dataframes

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -8,12 +8,8 @@ def load_data(path):
 def clean_data(data):
     """Nettoie les données en supprimant les colonnes inutiles et en gérant les valeurs manquantes."""
     # Suppression des colonnes inutiles
-<<<<<<< HEAD
-    data.drop(['Name', 'Ticket', 'Cabin', 'PassengerId'], axis=1)
-=======
     data.drop(['Name', 'Ticket', 'PassengerId'], axis=1, inplace=True)
     data["Cabin"] = data["Cabin"].apply(lambda s : s[0] if pd.notnull(s) else np.nan)
->>>>>>> d63520a4079a6a64e9b0296da65eb262f8c6aaf2
 
     # Gestion des valeurs manquantes
     num_cols = data.select_dtypes(include=[float, int]).columns
@@ -21,18 +17,10 @@ def clean_data(data):
 
     for col in num_cols:
         median_val = data[col].median()
-<<<<<<< HEAD
-        data[col].fillna(median_val)
-
-    for col in cat_cols:
-        freq_val = data[col].mode()[0]
-        data[col].fillna(freq_val)
-=======
         data[col] = data[col].fillna(median_val)
 
     for col in cat_cols:
         freq_val = data[col].mode()[0]
         data[col] = data[col].fillna(freq_val)
->>>>>>> d63520a4079a6a64e9b0296da65eb262f8c6aaf2
 
     return data
